0b68fbdc01af9ac148a12333d94ae5e2f01acd2d
Vasil Uhnyuck <vuhnyuc2@ncsu.edu>
2017-07-21 00:04:33 -0400

    Jira.js changes

--- jira.js
+++ jira.js
@@ -19,21 +19,52 @@ var client = new Client();
           console.log("received cookie");
           var session = data.session;
           credentials = session.name + "=" + session.value;
-	  done();
+	         done();
         }
       });
     }
   }
-  exports.get_users_issues = function(user, done){
-    console.log(credentials);
+
+  exports.create_story = function(summary, description, issueType, assignee, priority){
     var search_args = {
       headers : {
         'cookie' : credentials,
         'Content-Type' : 'application/json'
       },
       data : {
-        'jql' : 'status=done',
-	'maxResults' : '1'
+        "fields": {
+           "project":
+          {
+            "key": 'TSB-46'
+          },
+           "summary": summary,
+           "description": description,
+          //We can pass in an assignee object through get_user
+           "assignee": {
+             "name" : assignee
+           }
+         }
+       }
+     }
+     client.post("https://scrumtj.atlassian.net/rest/api/2/issue", search_args, function(data, response){
+       if(response.statusCode == 200){
+         done(data);
+       }
+       else{
+         done(null,response);
+       }
+     });
+   }
+
+  //Returns all stories for user
+  exports.get_stories_person = function(person, done){
+    var search_args = {
+      headers : {
+        'cookie' : credentials,
+        'Content-Type' : 'application/json'
+      },
+      data : {
+        'jql' : 'assignee in ('+person+')';
       }
     }
     client.post("https://scrumtj.atlassian.net/rest/api/2/search", search_args, function(data, response){
@@ -46,7 +77,150 @@ var client = new Client();
     });
   }
 
-  exports.set_task_status = function(){
-
+  //Might not be needed if get_user works as intended
+  exports.get_team = function(done){
+    var search_args = {
+      headers : {
+        'cookie' : credentials,
+        'Content-Type' : 'application/json'
+      },
+      data : {
+        'groupname' : 'site-admins';
+      }
+    }
+    client.get("https://scrumtj.atlassian.net/rest/api/2/group/member", search_args, function(data, response){
+      if(response.statusCode == 200){
+        done(data);
+      }
+      else{
+        done(null,response);
+      }
+    });
   }
 
+  //You may use this by searching for user by full name and then using the username of the data returned for
+  //further rest calls
+  exports.get_user = function(name, done){
+    var search_args = {
+      headers : {
+        'cookie' : credentials,
+        'Content-Type' : 'application/json'
+      },
+      data : {
+        'username' : name;
+      }
+    }
+    client.get("https://scrumtj.atlassian.net/rest/api/2/user/search", search_args, function(data, response){
+      if(response.statusCode == 200){
+        done(data);
+      }
+      else{
+        done(null,response);
+      }
+    });
+  }
+
+  exports.get_num_stories_done = function(done){
+    var search_args = {
+      headers : {
+        'cookie' : credentials,
+        'Content-Type' : 'application/json'
+      },
+      data : {
+        'jql' : 'status=done'
+      }
+    }
+    client.post("https://scrumtj.atlassian.net/rest/api/2/search", search_args, function(data, response){
+      if(response.statusCode == 200){
+        done(data.total);
+      }
+      else{
+        done(null,response);
+      }
+    });
+  }
+
+  exports.get_num_stories_doing = function(done){
+    var search_args = {
+      headers : {
+        'cookie' : credentials,
+        'Content-Type' : 'application/json'
+      },
+      data : {
+        'jql' : 'status="In Progress"'
+      }
+    }
+    client.post("https://scrumtj.atlassian.net/rest/api/2/search", search_args, function(data, response){
+      if(response.statusCode == 200){
+        done(data.total);
+      }
+      else{
+        done(null,response);
+      }
+    });
+  }
+
+  exports.get_num_stories_todo = function(done){
+    var search_args = {
+      headers : {
+        'cookie' : credentials,
+        'Content-Type' : 'application/json'
+      },
+      data : {
+        'jql' : 'status="To Do"'
+      }
+    }
+    client.post("https://scrumtj.atlassian.net/rest/api/2/search", search_args, function(data, response){
+      if(response.statusCode == 200){
+        done(data.total);
+      }
+      else{
+        done(null,response);
+      }
+    });
+  }
+
+  exports.set_task_status = function(task, status, done){
+    var search_args = {
+      headers : {
+        'cookie' : credentials,
+        'Content-Type' : 'application/json'
+      },
+      data : {
+        "transition": {
+          "id": status
+        }
+      }
+    }
+    client.post("https://scrumtj.atlassian.net/rest/api/2/issue/"+task+"/transitions", search_args, function(data, response){
+      if(response.statusCode == 200){
+        done(data);
+      }
+      else{
+        done(null,response);
+      }
+    });
+  }
+
+  //TEST NOT GOING TO BE USED IN FINAL
+  exports.get_users_issues = function(user, done){
+    console.log(credentials);
+    var search_args = {
+      headers : {
+        'cookie' : credentials,
+        'Content-Type' : 'application/json'
+      },
+      data : {
+        'jql' : 'status=done',
+	      'maxResults' : '1'
+      }
+    }
+    client.post("https://scrumtj.atlassian.net/rest/api/2/search", search_args, function(data, response){
+      if(response.statusCode == 200){
+        done(data);
+      }
+      else{
+        done(null,response);
+      }
+    });
+  }
